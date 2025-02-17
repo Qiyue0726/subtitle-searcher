5dc0419c1b613399f6b08a4412d3756a2ad99cff
Sakura <1301616594@qq.com>
2025-02-17 20:25:10 +0800

    winform(.net framework) + webview2

---
+++ winform-webview2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace winform_webview2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Margin = new System.Windows.Forms.Padding(0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(810, 637);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 634);
+            this.Controls.Add(this.webView21);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "字幕搜索";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ winform-webview2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace winform_webview2
+{
+
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public partial class Form1: Form
+    {
+        // 推荐将 HttpClient 设为静态以重用（避免端口耗尽）
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckWebView2Installation();
+            InitializeAsync();
+        }
+
+        async void CheckWebView2Installation()
+        {
+            try
+            {
+                await EnsureWebView2Async();
+                //MessageBox.Show("WebView2 已成功安装。");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"WebView2 未安装或初始化失败：{ex.Message}");
+            }
+        }
+
+        async Task EnsureWebView2Async()
+        {
+            var webView2 = new WebView2();
+            await webView2.EnsureCoreWebView2Async(null);
+        }
+
+        async void InitializeAsync()
+        {
+            // 初始化WebView2
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.AddHostObjectToScript("winFormProxy", this);
+
+            // 加载网页
+            //webView21.CoreWebView2.Navigate("https://www.baidu.com");
+
+            // 获取项目输出目录
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // 构建网页文件的完整路径
+            string htmlFilePath = Path.Combine(baseDirectory, "assets/index.html");
+
+            // 加载本地网页
+            webView21.CoreWebView2.Navigate(htmlFilePath);
+        }
+
+        [System.Runtime.InteropServices.ComVisible(true)]
+        public async Task<string> MakeApiRequest(string apiUrl)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        [System.Runtime.InteropServices.ComVisible(true)]
+        public async Task<string> Download(string url, string name)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                byte[] bytes = await response.Content.ReadAsByteArrayAsync();
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, name);
+                File.WriteAllBytes(filePath, bytes);
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        [System.Runtime.InteropServices.ComVisible(true)]
+        public void OpenFolder()
+        {
+            // 打开当前程序文件夹
+            Process.Start("explorer.exe", Application.StartupPath);
+        }
+    }
+}
